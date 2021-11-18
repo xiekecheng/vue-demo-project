76d45d25990066c6eb34f718d04e2e2092f5b797
xiekecheng <xkccoding@gmail.com>
2021-11-18 22:49:32 +0800

    引入normalize.css，初始化浏览器默认样式

--- src/main.js
+++ src/main.js
@@ -5,20 +5,21 @@
  * @LastEditTime: 2021-09-15 22:53:06
  * @FilePath: /vue-demo-project/src/main.js
  */
-import Vue from "vue";
-import App from "./App.vue";
-import ElementUI from "element-ui";
-import store from "@/store";
+import Vue from 'vue';
+import App from './App.vue';
+import ElementUI from 'element-ui';
+import store from '@/store';
 import request from '@/utils/axios';
 // 引入css样式
-import "element-ui/lib/theme-chalk/index.css";
+import 'element-ui/lib/theme-chalk/index.css';
+import 'normalize.css';
 Vue.config.productionTip = false;
-Vue.prototype.$request=request
+Vue.prototype.$request = request;
 // 引入elementUI
 Vue.use(ElementUI);
-import router from "@/router";
+import router from '@/router';
 new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
