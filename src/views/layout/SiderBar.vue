988ccedc6b4753309a8767503a002fd88a64784a
xiekecheng <xkccoding@gmail.com>
2021-11-27 23:30:21 +0800

    添加dashboard

--- src/views/layout/SiderBar.vue
+++ src/views/layout/SiderBar.vue
@@ -1,19 +1,34 @@
 <template>
-  <div>
+  <div class="my-siderbar">
     <!-- 侧边栏 开始 -->
-    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+    <!-- <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
       <el-radio-button :label="false">展开</el-radio-button>
       <el-radio-button :label="true">收起</el-radio-button>
-    </el-radio-group>
+    </el-radio-group> -->
     <el-menu
       default-active="1-4-1"
       class="el-menu-vertical-demo"
-      :collapse="isCollapse"
+      background-color="#ffffff"
       @open="handleOpen"
       @close="handleClose"
     >
+      <el-submenu index="0">
+        <template #title>
+          <i class="el-icon-odometer"></i>
+          <span slot="title">DashBoard</span>
+        </template>
+        <router-link to="/dashboard/analysis">
+          <el-menu-item index="0-1"> 分析页 </el-menu-item>
+        </router-link>
+        <router-link to="/dashboard/monitor">
+          <el-menu-item index="0-2"> 监控页 </el-menu-item>
+        </router-link>
+        <router-link to="/dashboard/workplace">
+          <el-menu-item index="0-3"> 工作台 </el-menu-item>
+        </router-link>
+      </el-submenu>
       <el-submenu index="1">
-        <template v-slot:title>
+        <template #title>
           <i class="el-icon-location"></i>
           <span slot="title">商品管理</span>
         </template>
@@ -35,20 +50,18 @@
       </el-submenu>
 
       <el-submenu index="2">
-        <template v-slot:title>
+        <template #title>
           <i class="el-icon-menu"></i>
           <span slot="title">图表管理</span>
         </template>
         <router-link :to="{ name: 'charts' }" tag="span">
           <el-menu-item index="2-1">图表</el-menu-item>
         </router-link>
-        <!-- <router-link :to="{ name: 'charts' }"> -->
         <el-menu-item index="2-2">选项2</el-menu-item>
-        <!-- </router-link> -->
       </el-submenu>
 
       <el-submenu index="3">
-        <template v-slot:title>
+        <template #title>
           <i class="el-icon-document"></i>
           <span slot="title">商品管理</span>
         </template>
@@ -59,7 +72,7 @@
       </el-submenu>
 
       <el-submenu index="4">
-        <template v-slot:title>
+        <template #title>
           <i class="el-icon-setting"></i>
           <span slot="title">测试页面</span>
         </template>
@@ -88,7 +101,7 @@
 export default {
   data() {
     return {
-      isCollapse: true,
+      isCollapse: false,
     };
   },
   methods: {
@@ -106,7 +119,15 @@ export default {
 /* 侧边栏 开始 */
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
-  min-height: 400px;
+  // min-height: 400px;
+  min-height: 100%;
 }
 /* 侧边栏 结束 */
+
+.my-siderbar {
+  height: 100%;
+}
+* {
+  text-decoration: none;
+}
 </style>
