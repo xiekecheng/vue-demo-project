49debc348d9188e89ca67a8668deb63a7c9df220
xkccoding <xkccoding@gmail.com>
2022-01-04 22:33:51 +0800

    update

--- src/views/exception/403.vue
+++ src/views/exception/403.vue
@@ -1,15 +1,21 @@
 <template>
-	<div>
-		<h1>403</h1>
-	</div>
+  <div>
+    <h1>403</h1>
+    <el-result title="403" subTitle="抱歉，请求错误">
+      <template slot="icon">
+        <el-image
+          src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png"
+        ></el-image>
+      </template>
+      <template slot="extra">
+        <el-button type="primary" size="medium">返回</el-button>
+      </template>
+    </el-result>
+  </div>
 </template>
 
 <script>
-	export default {
-		
-	}
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
