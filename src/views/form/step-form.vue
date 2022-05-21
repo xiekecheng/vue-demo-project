3f9a5b0b28a1780a54e014c64ef1c64bfa7767bd
xkccoding <xkccoding@gmail.com>
2022-05-21 14:44:42 +0800

    ✨ feat(表单->分布表单): 新增betterScroll的demo

--- src/views/form/step-form.vue
+++ src/views/form/step-form.vue
@@ -1,15 +1,31 @@
 <template>
-	<div>
-		<h1>分步表单</h1>
-	</div>
+  <div>
+    <h1>分步表单</h1>
+    <div ref="box" class="wrap">
+      <div>
+        <ul>
+          <li v-for="i in 20" :key="i">1111</li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		
-	}
+import BetterScroll from 'better-scroll';
+export default {
+  mounted() {
+		const wrap = document.querySelector('.wrap')
+    // eslint-disable-next-line no-unused-vars
+    const bs = new BetterScroll(wrap);
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+.wrap {
+  height: 300px;
+  background-color: pink;
+  overflow: hidden;
+}
 </style>
