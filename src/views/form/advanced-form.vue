ec3a23f5d4548b09081c76665f6f0286a05796ac
xkccoding <xkccoding@gmail.com>
2022-06-13 23:14:38 +0800

    ✨ feat: 根据传入的数据嵌套递归组件

--- src/views/form/advanced-form.vue
+++ src/views/form/advanced-form.vue
@@ -1,15 +1,34 @@
 <template>
-	<div>
-		<h1>高级表单</h1>
-	</div>
+  <div>
+    <h1>高级表单</h1>
+    <MyTable :TreeData="list" />
+  </div>
 </template>
 
 <script>
-	export default {
-		
-	}
+import MyTable from './components/Table.vue';
+export default {
+  components: {
+    MyTable,
+  },
+  data() {
+    return {
+      list: [
+        { value: 1, children: [{ value: 5 }] },
+        { value: 2 },
+        {
+          value: 3,
+          children: [
+            {
+              value: 6,
+            },
+          ],
+        },
+        { value: 4 },
+      ],
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
---
+++ src/views/form/components/Table.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<div v-for="(item,index) in TreeData" :key="index">
+			<span>{{item.value}}</span>
+			<div v-if="item.children">
+				<MyList :TreeData="item.children"></MyList>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'MyList',
+		props:{
+			TreeData:{
+				type:Array,
+				default:()=>[]
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
