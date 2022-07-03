4bdf74e34313f4de2e2967e428b8b2756d026259
xkccoding <xkccoding@gmail.com>
2022-07-03 16:26:09 +0800

    ✨ feat(个人中心页): 完成右侧信息栏-项目

--- src/views/account/center.vue
+++ src/views/account/center.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <el-row :gutter="24">
-      <el-col :md="7" :lg="7" :xl="12">
+      <el-col :md="7" :lg="7" :xl="7">
         <div class="grid-content bg-purple">
           <div class="top-content">
             <div class="avatar">
@@ -44,26 +44,105 @@
             </el-input>
             <el-button v-else class="button-new-tag" size="mini" @click="showInput">+</el-button>
           </div>
-          <div class="bottom-content"></div>
+          <div class="bottom-content">
+            <h6>团队</h6>
+            <el-row :gutter="10">
+              <el-col :lg="12" :xl="12"
+                ><div class="group">
+                  <span class="icon"
+                    ><img src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png" alt height="24"
+                  /></span>
+                  科学搬砖组
+                </div></el-col
+              >
+              <el-col :lg="12" :xl="12"
+                ><div class="group">
+                  <span class="icon"
+                    ><img
+                      src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png"
+                      alt
+                      height="24" /></span
+                  >全组都是吴彦祖
+                </div></el-col
+              >
+              <el-col :lg="12" :xl="12"
+                ><div class="group">
+                  <span class="icon"
+                    ><img
+                      src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png"
+                      alt
+                      height="24" /></span
+                  >中二少女团
+                </div></el-col
+              >
+              <el-col :lg="12" :xl="12"
+                ><div class="group">
+                  <span class="icon"
+                    ><img
+                      src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png"
+                      alt
+                      height="24" /></span
+                  >程序员日常
+                </div></el-col
+              >
+              <el-col :lg="12" :xl="12"
+                ><div class="group">
+                  <span class="icon"
+                    ><img
+                      src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png"
+                      alt
+                      height="24" /></span
+                  >高逼格设计天团
+                </div></el-col
+              >
+              <el-col :lg="12" :xl="12"
+                ><div class="group">
+                  <span class="icon"
+                    ><img
+                      src="https://gw.alipayobjects.com/zos/rmsportal/WdGqmHpayyMjiEhcKoVE.png"
+                      alt
+                      height="24" /></span
+                  >骗你来学计算机
+                </div></el-col
+              >
+            </el-row>
+          </div>
         </div>
       </el-col>
-      <el-col :md="17" :lg="17" :xl="12">
-        <div class="grid-content bg-purple-light"></div>
+      <el-col :md="17" :lg="17" :xl="17">
+        <div class="article">
+          <el-tabs v-model="activeName" @tab-click="handleClick">
+            <el-tab-pane label="文章(8)" name="first">
+              <el-empty :image-size="200"></el-empty>
+            </el-tab-pane>
+            <el-tab-pane label="应用(8)" name="second">配置管理</el-tab-pane>
+            <el-tab-pane label="项目(8)" name="third"> <Project /> </el-tab-pane>
+          </el-tabs>
+        </div>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
+import Project from './components/Project';
 export default {
+  components: {
+    Project,
+  },
   data() {
     return {
       dynamicTags: ['很有想法的', '专注设计', '辣~', '大长腿', '川妹子'],
       inputVisible: false,
       inputValue: '',
+      activeName: 'third',
+      currentDate: new Date(),
     };
   },
   methods: {
+    handleClick(tab, event) {
+      console.log(tab, event);
+    },
     showInput() {
       this.inputVisible = true;
       this.$nextTick((_) => {
@@ -86,6 +165,10 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.article{
+  background-color: #ffffff;
+  padding: 12px;
+}
 .grid-content {
   background-color: #ffffff;
   min-height: 400px;
@@ -113,13 +196,25 @@ export default {
   border-top: 1px dashed rgba(0, 0, 0, 0.06);
   border-bottom: 1px dashed rgba(0, 0, 0, 0.06);
 }
+.bottom-content {
+  .group {
+    line-height: 28px;
+  }
+  .icon {
+    border-radius: 50%;
+    display: inline-block;
+    overflow: hidden;
+    height: 24px;
+    line-height: 24px;
+  }
+}
 .input-new-tag {
   width: 90px;
   margin-left: 10px;
   vertical-align: bottom;
   height: 20px;
   margin-bottom: 5px;
-  ::v-deep .el-input__inner{
+  ::v-deep .el-input__inner {
     height: 20px !important;
   }
 }
@@ -134,4 +229,17 @@ export default {
   margin-left: 8px;
   margin-bottom: 5px;
 }
+
+
+// .bottom {
+//   margin-top: 13px;
+//   line-height: 12px;
+// }
+
+// .button {
+//   padding: 0;
+//   float: right;
+// }
+
+
 </style>
