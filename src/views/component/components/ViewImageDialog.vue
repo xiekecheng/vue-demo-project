a60e9a6e06df47148f286fa036c8a04a95d9c29d
xkccoding <xkccoding@gmail.com>
2022-08-14 10:18:57 +0800

    ✨ feat: 预览图片大图弹窗

---
+++ src/views/component/components/ViewImageDialog.vue
@@ -0,0 +1,27 @@
+<!-- 查看图片对话框 -->
+<template>
+  <div>
+    <el-dialog title="查看图片" :visible.sync="dialogVisible" width="50%" destroy-on-close close-on-click-modal >
+      <el-image style="width: 500px; height: 500px" :src="url" fit="fill"></el-image>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+	props:{
+		url:{
+			type: String,
+		}
+	},
+  data() {
+    return {
+      dialogVisible: false,
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
