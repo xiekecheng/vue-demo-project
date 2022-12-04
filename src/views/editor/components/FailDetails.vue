985d94831bfcf3857e2a0be2528fbdcb02177a3a
xkccoding <xkccoding@gmail.com>
2022-12-04 23:58:07 +0800

    feat:1.进度条 2.查看失败详情

---
+++ src/views/editor/components/FailDetails.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <el-dialog :title='title' :visible.sync='dialogVisible'>
+      <el-table :data='tableData' stripe max-height='440' size='small'>
+        <el-table-column v-for='(item,index) in tableColumns' :key='index' :property='item.property'
+                         :label='item.label' />
+      </el-table>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialogVisible: false,
+      tableColumns: [],
+      tableData: [],
+      title: '',
+    };
+  },
+  methods: {
+    // 显示错误信息
+    showFailDetails(tableData, tableColumns, title = '失败详情') {
+      this.tableData = tableData;
+      this.tableColumns = tableColumns;
+      this.title = title;
+      this.dialogVisible = true;
+    },
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+
+</style>
